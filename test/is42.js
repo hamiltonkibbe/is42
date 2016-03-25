b2d1b38b953b9f2630cf489431d74e38756142c9
Hamilton Kibbe <ham@hamiltonkib.be>
2016-03-25 10:59:33 -0400

    Initial Commit

---
+++ test/is42.js
@@ -0,0 +1,46 @@
+
+
+
+var should = require('chai').should();
+var is42 = require('../is42');
+
+
+describe('#Handling of 42', function() {
+    it('Verifies that 42 is 42', function() {
+        is42.is42(42).should.equal(true);   
+    });
+
+    it('Verifies that 42.0 is 42', function() {
+        is42.is42(42.0).should.equal(true);
+    });
+
+    it("verifies that '42' is 42", function() {
+        is42.is42('42').should.equal(true);
+    })
+
+    it('verifies that "fortytwo" is 42', function() {
+        is42.is42('fortytwo').should.equal(true);
+    });
+
+    it('verifies that "forty two" is 42', function() {
+        is42.is42('forty two').should.equal(true);
+    });
+
+    it('verifies that "forty-two" is 42', function() {
+        is42.is42('forty-two').should.equal(true);
+    })
+});
+
+describe('#Handling of things other than 42', function() { 
+    it('Verifies that 55 is not 42', function() {
+        is42.is42(55).should.equal(false);
+    });
+
+    it('Verifies that null is not 42', function() {
+        is42.is42(null).should.equal(false);
+    });
+
+    it('Verifies that undefined is not 42', function() {
+        is42.is42(undefined).should.equal(false);
+    });
+});
